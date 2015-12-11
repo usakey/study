31fcb508919b06851260028c16e51944d9be5633
usakey <rdxiaolei@gmail.com>
2015-12-11 15:21:31 +0800

    add Map reduce for inverted index

---
+++ datasci_coursera/assignment 3-Map Reduce/inverted_index.py
@@ -0,0 +1,33 @@
+import MapReduce
+import sys
+
+"""
+Word Count Example in the Simple Python MapReduce Framework
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: document identifier
+    # value: document contents
+    key = record[0]
+    value = record[1]
+    words = value.split()
+    for w in words:
+      mr.emit_intermediate(w, key)
+
+def reducer(key, list_of_values):
+    # key: word
+    # value: list of appended docid
+    ## remove duplicates via set()
+    values_without_dup = list(set(list_of_values))
+    mr.emit((key, values_without_dup))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
